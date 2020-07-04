099d3f0bb69f327c1693fbb14370c19bb9b2c2a6
mohitpawar10 <mohit@brandmovers.co.uk>
2020-07-04 10:55:05 +0545

    Removed unwanted code for merge.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,7 +14,6 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -23,29 +22,11 @@ from sklearn.naive_bayes import GaussianNB
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-""" compute the accuracy of your Naive Bayes classifier """
 
-### create classifier
-clf = GaussianNB()
 
-t_train = time()
+#########################################################
+### your code goes here ###
 
-### fit the classifier on the training features and labels
-clf.fit(features_train, labels_train)
 
-print "Training time:", round(time()-t_train, 3), "s"
+#########################################################
 
-t_predict = time()
-
-### use the trained classifier to predict labels for the test features
-pred = clf.predict(features_test[0])
-
-print "Prediction time:", round(time()-t_predict, 3), "s"
-
-### calculate and return the accuracy on the test data
-### this is slightly different than the example, 
-### where we just print the accuracy
-### you might need to import an sklearn module
-accuracy = clf.score(features_test, labels_test)
-
-print(accuracy)
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,13 +7,11 @@
     Sara has label 0
     Chris has label 1
 """
-
+    
 import sys
 from time import time
 sys.path.append("../tools/")
-
 from email_preprocess import preprocess
-from sklearn import svm
 
 
 ### features_train and features_test are the features for the training
@@ -28,25 +26,4 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
-clf = svm.SVC(kernel = "linear");
 
-t_train = time()
-
-clf.fit(features_train, labels_train)
-
-print "Training time:", round(time()-t_train, 3), "s"
-
-t_predict = time()
-
-### use the trained classifier to predict labels for the test features
-pred = clf.predict(features_test[0])
-
-print "Prediction time:", round(time()-t_predict, 3), "s"
-
-### calculate and return the accuracy on the test data
-### this is slightly different than the example, 
-### where we just print the accuracy
-### you might need to import an sklearn module
-accuracy = clf.score(features_test, labels_test)
-
-print(accuracy)
